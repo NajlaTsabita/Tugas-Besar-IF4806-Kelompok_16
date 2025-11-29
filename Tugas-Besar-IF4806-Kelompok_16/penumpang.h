65986f37f2f54fc0850d242ba8d49f8bf27a0262
NajlaTsabita <najlatsabitaafiyah@gmail.com>
2025-11-29 11:28:33 +0700

    buat headers

--- Tugas-Besar-IF4806-Kelompok_16/penumpang.h
+++ Tugas-Besar-IF4806-Kelompok_16/penumpang.h
@@ -1,6 +1,20 @@
 #ifndef PENUMPANG_H_INCLUDED
 #define PENUMPANG_H_INCLUDED
+#include <iostream>
 
+using namespace std;
 
+struct infotypeP {
+    string nama;
+    string noKTP;
+    int umur;
+};
+typedef Penumpang* adrPenumpang;
+struct Penumpang {
+    infotypeP info;
+    adrPenumpang next;
+};
+
+void createListBis(ListBis &L);
 
 #endif // PENUMPANG_H_INCLUDED
